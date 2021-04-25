1641416ca2521f076fef1d4abea88ff631689ab1
sohee <cwc06129@naver.com>
2021-04-25 19:08:29 +0900

    delete background modify

--- Feature Point Detection/background2.py
+++ Feature Point Detection/background2.py
@@ -3,10 +3,9 @@ from numpy import percentile
 import numpy as np
 
 img = cv2.imread('background.jpg', cv2.IMREAD_GRAYSCALE)
-lower = percentile(img, 5)
+lower = percentile(img, 0)
 upper = percentile(img, 100)
-cv2.normalize(img, img, -lower, 800+upper, cv2.NORM_MINMAX)
-# cv2.normalize(img, img, -lower, 100+upper, cv2.NORM_MINMAX)
+cv2.normalize(img, img, 0, 550+upper, cv2.NORM_MINMAX)
 cv2.imwrite('output.jpg', img)
 sub_dst = cv2.subtract(img, 100)
 cv2.imwrite('back2.jpg', sub_dst)
